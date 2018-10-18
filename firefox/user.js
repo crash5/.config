30ea82e53e3357057321a6b688db50ad6705f839
Imre NAGY <crash5@users.noreply.github.com>
2018-10-18 23:29:42 +0200

    Firefox: add user.js

---
+++ firefox/user.js
@@ -0,0 +1,16 @@
+user_pref("extensions.pocket.enabled", false);
+user_pref("loop.enabled", false);
+user_pref("media.autoplay.enabled", false);
+user_pref("browser.tabs.closeWindowWithLastTab", false);
+
+// > Settings menu entries
+user_pref("accessibility.force_disabled", 1);
+user_pref("app.update.auto", false);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.update", false);
+user_pref("javascript.use_us_english_locale", true);
+
+user_pref("services.sync.engine.passwords", false);
+user_pref("services.sync.engine.prefs.modified", false);
+user_pref("services.sync.engine.tabs", false);
+// < Settings menu entries
