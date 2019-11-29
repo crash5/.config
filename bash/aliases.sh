9f26029951909e6e54c4e250582885c1b64edf27
Imre NAGY <crash5@users.noreply.github.com>
2019-11-29 09:01:21 +0100

    bash: add mkcd and ..2/3/4 aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -2,12 +2,14 @@ alias cp='cp -i'
 alias mv='mv -i'
 alias ll='ls -lahF --color=always --group-directories-first'
 alias ..='cd ..'
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
 alias -- -='cd -'
 alias df='df -h'
 #alias du='du -hsL'
 alias vi='vim'
 alias openports='netstat -nape --inet'
-alias finf='grep -rl . -e'
 alias shareonhttp='python -m SimpleHTTPServer'
 alias mc='. /usr/lib/mc/mc-wrapper.sh -x'
 alias rl='readlink -m'
@@ -20,4 +22,8 @@ complete -o bashdefault -o default -o nospace -F __git_wrap__git_main  g
 
 alias k='kubectl'
 complete -o default -F __start_kubectl k
+
+mkcd() {
+    mkdir -p "${1:? need a directory}" && cd "${1}"
+}
 
