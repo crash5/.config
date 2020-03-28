44fe31ba9e9a0c5bcc27c2c46b632b1a6231e475
Imre NAGY <crash5@users.noreply.github.com>
2020-03-28 15:14:10 +0100

    bash: fix fzf completion

--- bash/bashrc.d/150.xdg-dirs.sh
+++ bash/bashrc.d/150.xdg-dirs.sh
@@ -3,3 +3,5 @@ export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
 export XDG_BIN_HOME=${XDG_BIN_HOME:-$HOME/.local/bin}
 export XDG_LIB_HOME=${XDG_LIB_HOME:-$HOME/.local/lib}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+
+export PATH="$PATH:$XDG_BIN_HOME"
