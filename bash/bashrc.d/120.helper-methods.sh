cac1eaba9dfedf7f2ec5c83e5299bd718f5a094d
Imre NAGY <crash5@users.noreply.github.com>
2020-03-28 16:50:32 +0100

    bash: remove export from helper methods

--- bash/bashrc.d/120.helper-methods.sh
+++ bash/bashrc.d/120.helper-methods.sh
@@ -2,10 +2,9 @@ function_exists() {
     declare -f -F $1 > /dev/null
     return $?
 }
-export -f function_exists
+
 
 program_exists() {
     which $1 >/dev/null 2>&1
     return $?
 }
-export -f program_exists
