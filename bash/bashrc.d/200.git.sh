118374946d958f649b04a09910020e7bbf761870
Imre NAGY <crash5@users.noreply.github.com>
2020-03-28 17:11:12 +0100

    bash: merge git completion and settings, fix completion in msys git

--- bash/bashrc.d/200.git.sh
+++ bash/bashrc.d/200.git.sh
@@ -1,9 +1,11 @@
+export GIT_PS1_SHOWDIRTYSTATE=1
+export GIT_PS1_SHOWUPSTREAM="auto"
+
 if program_exists git; then
     g() {
         if [[ $# == 0 ]]; then git st; else git "$@"; fi
     }
-    if function_exists _completion_loader; then
-        _completion_loader git
-        complete -o bashdefault -o default -o nospace -F __git_wrap__git_main  g
-    fi
+
+    function_exists __git_wrap__git_main || _completion_loader git
+    function_exists __git_wrap__git_main && complete -o bashdefault -o default -o nospace -F __git_wrap__git_main g
 fi
