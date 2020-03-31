47f90086eef6050c73cd09e8d11c215a0db68376
Imre NAGY <crash5@users.noreply.github.com>
2020-03-31 14:10:09 +0200

    bash: use exa for ll, llt and llst if available instead of ls

---
+++ bash/bashrc.d/210.exa-for-ll.sh
@@ -0,0 +1,5 @@
+if program_exists exa; then
+    alias ll='exa --long --group-directories-first --all --all --git'
+    alias llt='exa --long --group-directories-first --all --git --tree'
+    alias llst='exa --group-directories-first --all --tree'
+fi
