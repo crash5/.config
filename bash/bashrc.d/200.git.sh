b178efae84a474af0bf3e0202c1088aa40f9b11d
Imre NAGY <crash5@users.noreply.github.com>
2020-04-03 08:08:05 +0200

    bash: fix git alias operator grouping

--- bash/bashrc.d/200.git.sh
+++ bash/bashrc.d/200.git.sh
@@ -6,6 +6,6 @@ if program_exists git; then
         if [[ $# == 0 ]]; then git st; else git "$@"; fi
     }
 
-    function_exists __git_wrap__git_main || (function_exists _completion_loader && _completion_loader git)
+    function_exists __git_wrap__git_main || { function_exists _completion_loader && _completion_loader git ; }
     function_exists __git_wrap__git_main && complete -o bashdefault -o default -o nospace -F __git_wrap__git_main g
 fi
