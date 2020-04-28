d31d0e837af376cd40b23aadb24f7ec90ade7730
Imre NAGY <crash5@users.noreply.github.com>
2020-04-28 17:51:16 +0200

    bash: open source dir in tmux when in WSL

--- bash/bashrc.d/200.tmux-on-login.sh
+++ bash/bashrc.d/200.tmux-on-login.sh
@@ -1,6 +1,8 @@
 # attach to running tmux or create new with name 'default'
 if [ -z "$TMUX" ] && [ ! -z "$(type -P tmux)" ]
 then
-    tmux new-window -t default -c "$PWD"
+    if [[ "$(</proc/sys/kernel/osrelease)" == *Microsoft* ]]; then
+        tmux new-window -t default -c "$PWD"
+    fi
     tmux attach -t default || tmux new -s default
 fi
