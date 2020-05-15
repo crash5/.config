d093ec3a51b5a0aba882fe0420a102d4dd23393d
Imre NAGY <crash5@users.noreply.github.com>
2020-05-15 15:36:01 +0200

    bash: use helm3 for alias h if available

--- bash/bashrc.d/200.helm.sh
+++ bash/bashrc.d/200.helm.sh
@@ -1,6 +1,12 @@
-if program_exists helm; then
+if program_exists helm3; then
+    alias h='helm3'
+
+    source <(helm3 completion bash)
+    complete -o default -F __start_helm h
+elif program_exists helm; then
     alias h='helm'
 
     source <(helm completion bash)
     complete -o default -F __start_helm h
 fi
+
