a8b3f451ff62f92e3e5a4a497d7a678054c282e2
Imre NAGY <crash5@users.noreply.github.com>
2020-05-25 10:18:44 +0200

    bash: add commented out history reload after every command

--- bash/bashrc.d/130.bash-save-history-after-commands.sh
+++ bash/bashrc.d/130.bash-save-history-after-commands.sh
@@ -1,3 +1,7 @@
+# if [[ ${PROMPT_COMMAND} != *"history -n"* ]]; then
+#     export PROMPT_COMMAND="history -n;${PROMPT_COMMAND}"
+# fi
+
 if [[ ${PROMPT_COMMAND} != *"history -a"* ]]; then
     export PROMPT_COMMAND="history -a;${PROMPT_COMMAND}"
 fi
