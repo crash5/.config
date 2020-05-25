1dc5d2108e20b6ba6300c56c29276ee49712e5e4
Imre NAGY <crash5@users.noreply.github.com>
2020-05-25 10:17:49 +0200

    bash: increase history size, store timestamp, leave dups in the list

--- bash/bashrc.d/130.bash-history-config.sh
+++ bash/bashrc.d/130.bash-history-config.sh
@@ -1,3 +1,6 @@
-export HISTCONTROL=ignoreboth:erasedups
-export HISTSIZE=10000
-export HISTFILESIZE=10000
+export HISTCONTROL=ignoreboth
+export HISTSIZE=30000
+export HISTFILESIZE=30000
+
+export HISTTIMEFORMAT='%Y-%m-%dT%H:%M:%S  '
+# export HISTIGNORE='ll:g:pwd:date'
