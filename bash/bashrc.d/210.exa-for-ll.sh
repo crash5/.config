e3513220bd422e7983d999b4edb328c2389c3980
Imre NAGY <crash5@users.noreply.github.com>
2020-09-11 07:54:17 +0200

    exa: add llsd alias to sort filelist by date

--- bash/bashrc.d/210.exa-for-ll.sh
+++ bash/bashrc.d/210.exa-for-ll.sh
@@ -1,5 +1,6 @@
 if program_exists exa; then
     alias ll='exa --long --group-directories-first --all --all --git'
     alias llt='exa --long --group-directories-first --all --git --tree'
+    alias llsd='ll -s date'
     alias llts='exa --group-directories-first --all --tree'
 fi
