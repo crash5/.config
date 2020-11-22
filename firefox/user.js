9ca5827965bc3ee921af4f7cad557932b272288a
Imre NAGY <crash5@users.noreply.github.com>
2020-11-22 12:21:43 +0100

    firefox: update with some new settigs

--- firefox/user.js
+++ firefox/user.js
@@ -8,12 +8,15 @@ user_pref("app.update.service.enabled", false);
 user_pref("app.update.silent", false);
 user_pref("app.update.staging.enabled", false);
 user_pref("breakpad.reportURL", "");
-user_pref("browser.aboutHomeSnippets.updateUrl", "data:,");
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.aboutHomeSnippets.updateUrl", "");
 user_pref("browser.altClickSave", true);
 user_pref("browser.bookmarks.editDialog.firstEditField", "tagsField");
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
 user_pref("browser.bookmarks.showMobileBookmarks", true);
 user_pref("browser.chrome.errorReporter.enabled", false);
 user_pref("browser.chrome.errorReporter.submitUrl", "");
+user_pref("browser.contentblocking.category", "strict");
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit", false); // (FF51-57)
 user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false); // (FF58+)
 user_pref("browser.crashReports.unsubmittedCheck.enabled", false); // (FF51+)
@@ -23,7 +26,14 @@ user_pref("browser.download.animateNotifications", false);
 user_pref("browser.eme.ui.firstContentShown", true);
 user_pref("browser.fixup.hide_user_pass", true);
 user_pref("browser.link.open_newwindow.restriction", 0);
+user_pref("browser.messaging-system.whatsNewPanel.enabled", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.snippets", false);
 user_pref("browser.newtabpage.activity-stream.feeds.telemetry", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
 user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
 user_pref("browser.newtabpage.activity-stream.telemetry", false);
 user_pref("browser.newtabpage.enabled", false);
@@ -41,6 +51,7 @@ user_pref("browser.safebrowsing.provider.google4.reportURL", ""); // (FF50+)
 user_pref("browser.safebrowsing.reportPhishURL", "");
 user_pref("browser.search.suggest.enabled", false);
 user_pref("browser.search.update", false);
+user_pref("browser.startup.homepage_override.mstone", "ignore")
 user_pref("browser.startup.page", 0);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("browser.tabs.crashReporting.sendReport", false);
@@ -49,10 +60,12 @@ user_pref("browser.tabs.warnOnClose", false);
 user_pref("browser.uiCustomization.state", "{\"placements\":{\"widget-overflow-fixed-list\":[],\"PersonalToolbar\":[\"personal-bookmarks\"],\"nav-bar\":[\"back-button\",\"forward-button\",\"stop-reload-button\",\"urlbar-container\",\"downloads-button\",\"ublock0_raymondhill_net-browser-action\",\"_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action\",\"umatrix_raymondhill_net-browser-action\"],\"toolbar-menubar\":[\"menubar-items\"],\"TabsToolbar\":[\"tabbrowser-tabs\",\"history-panelmenu\",\"new-tab-button\",\"privatebrowsing-button\",\"alltabs-button\"]},\"seen\":[\"developer-button\",\"ublock0_raymondhill_net-browser-action\",\"_73a6fe31-595d-460b-a920-fcc0f8843232_-browser-action\",\"webide-button\",\"umatrix_raymondhill_net-browser-action\"],\"dirtyAreaCache\":[\"PersonalToolbar\",\"nav-bar\",\"toolbar-menubar\",\"TabsToolbar\"],\"currentVersion\":16,\"newElementCount\":15}");
 user_pref("browser.uidensity", 1);
 user_pref("browser.urlbar.clickSelectsAll", true);
+user_pref("browser.urlbar.matchBuckets", "general:5,suggestion:Infinity");
 user_pref("browser.urlbar.maxRichResults", 25);
 user_pref("browser.urlbar.openViewOnFocus", false);
 user_pref("browser.urlbar.searchSuggestionsChoice", false);
 user_pref("browser.urlbar.suggest.searches", false);
+user_pref("browser.urlbar.suggest.topsites", false);
 user_pref("browser.urlbar.timesBeforeHidingSuggestionsHint", 0);
 user_pref("browser.urlbar.trimURLs", false);
 user_pref("datareporting.healthreport.uploadEnabled", false);
@@ -66,9 +79,11 @@ user_pref("devtools.webide.autoinstallADBHelper", false);
 user_pref("devtools.webide.enabled", false);
 user_pref("dom.ipc.plugins.reportCrashURL", false);
 user_pref("dom.push.enabled", false); // web push
+user_pref("dom.security.https_only_mode", true);
 user_pref("dom.serviceWorkers.enabled", false); // web push notifications
 user_pref("dom.webnotifications.enabled", false); // web push notifications
 user_pref("dom.webnotifications.serviceworker.enabled", false); // sites can still generate desktop notifications while you have a tab open to the site 
+user_pref("extensions.formautofill.addresses.enabled", false)
 user_pref("extensions.formautofill.creditCards.enabled", false); // (FF56+)
 user_pref("extensions.getAddons.cache.enabled", false);
 user_pref("extensions.getAddons.showPane", false); // hidden pref
@@ -99,28 +114,34 @@ user_pref("media.peerconnection.enabled", false);
 user_pref("media.webspeech.recognition.enable", false);
 user_pref("media.webspeech.synth.enabled", false);
 user_pref("network.allow-experiments", false);
+user_pref("network.captive-portal-service.enabled", false)
 user_pref("network.cookie.cookieBehavior", 4);
 user_pref("network.cookie.lifetimePolicy", 2);
 user_pref("network.dns.disablePrefetch", true);
 user_pref("network.http.speculative-parallel-limit", 0);
 user_pref("network.predictor.enabled", false);
 user_pref("network.prefetch-next", false);
+user_pref("network.trr.custom_uri", "https://doh.libredns.gr/dns-query");
 user_pref("permissions.default.camera", 2);
 user_pref("permissions.default.desktop-notification", 2);
 user_pref("permissions.default.geo", 2);
 user_pref("permissions.default.microphone", 2);
+user_pref("permissions.default.xr", 2);
 user_pref("privacy.donottrackheader.enabled", true);
 user_pref("privacy.spoof_english", 2);
 user_pref("privacy.trackingprotection.enabled", true);
 user_pref("reader.parse-on-load.enabled", false);
 user_pref("reader.parse-on-load.force-enabled", false);
 user_pref("security.dialog_enable_delay", 0);
-user_pref("services.sync.declinedEngines", "passwords,tabs");
+user_pref("security.disable_button.openCertManager", false);
+user_pref("services.sync.declinedEngines", "passwords,tabs,creditcards");
 user_pref("services.sync.engine.passwords", false);
 user_pref("services.sync.engine.prefs.modified", false);
 user_pref("services.sync.engine.tabs", false);
 user_pref("signon.autofillForms", false);
 user_pref("signon.autofillForms.http", false);
+user_pref("signon.generation.enabled", false);
+user_pref("signon.management.page.breach-alerts.enabled", false);
 user_pref("toolkit.cosmeticAnimations.enabled", false);
 user_pref("toolkit.telemetry.archive.enabled", false);
 user_pref("toolkit.telemetry.bhrPing.enabled", false);
@@ -129,10 +150,12 @@ user_pref("toolkit.telemetry.enabled", false);
 user_pref("toolkit.telemetry.firstShutdownPing.enabled", false);
 user_pref("toolkit.telemetry.hybridContent.enabled", false);
 user_pref("toolkit.telemetry.newProfilePing.enabled", false);
+user_pref("toolkit.telemetry.pioneer-new-studies-available", false);
 user_pref("toolkit.telemetry.reportingpolicy.firstRun", false);
 user_pref("toolkit.telemetry.server", "data:,");
 user_pref("toolkit.telemetry.shutdownPingSender.enabled", false);
 user_pref("toolkit.telemetry.unified", false);
 user_pref("toolkit.telemetry.updatePing.enabled", false);
+user_pref("trailhead.firstrun.didSeeAboutWelcome", true);
 user_pref("ui.SpellCheckerUnderlineStyle", 4);
 user_pref("_user.js.parrot", "Every user.js applyed!");
