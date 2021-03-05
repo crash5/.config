75a8660a55e57131baf8ef5a3443d90e1c504226
Imre NAGY <crash5@users.noreply.github.com>
2021-03-05 10:31:49 +0100

    bash: increate history size to 50000
    HISTFILESIZE='-1' means use the value from the HISTSIZE

--- bash/bashrc.d/130.bash-history-config.sh
+++ bash/bashrc.d/130.bash-history-config.sh
@@ -1,6 +1,6 @@
 export HISTCONTROL=ignoreboth
-export HISTSIZE=30000
-export HISTFILESIZE=30000
+export HISTSIZE=50000
+export HISTFILESIZE='-1'
 
 export HISTTIMEFORMAT='%Y-%m-%dT%H:%M:%S  '
 export HISTIGNORE='ll:g:pwd'
