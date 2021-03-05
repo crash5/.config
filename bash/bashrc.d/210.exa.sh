0ec311857d9e60790111bd7e3368ab8a7e2e2c3c
Imre NAGY <crash5@users.noreply.github.com>
2021-03-05 10:33:39 +0100

    exa: skip .git direrctory in llt/llts tree listing

--- bash/bashrc.d/210.exa.sh
+++ bash/bashrc.d/210.exa.sh
@@ -1,6 +1,6 @@
 if program_exists exa; then
     alias ll='exa --long --group-directories-first --all --all --git'
-    alias llt='exa --long --group-directories-first --all --git --tree'
+    alias llt='exa --long --group-directories-first --all --git --tree -I.git'
+    alias llts='exa --group-directories-first --all --tree -I.git'
     alias llsd='ll -s date'
-    alias llts='exa --group-directories-first --all --tree'
 fi
