126e5f68d4f873c58c80149497e19480fb15b65a
Imre NAGY <crash@veluna.eu>
2023-02-22 15:01:32 +0100

    vscode/neovim: handle ctrl+n/p use targets.vim

--- nvim/vscode-init.lua
+++ nvim/vscode-init.lua
@@ -14,6 +14,8 @@ vim.cmd [[
     augroup END
 
     " vscode only >
+    packadd! targets.vim
+
     set clipboard^=unnamed,unnamedplus
 
     nnoremap <leader>g <Cmd>call VSCodeNotify('workbench.action.findInFiles', { 'query': expand('<cword>')})<CR>
@@ -29,5 +31,10 @@ vim.cmd [[
     nmap gc  <Plug>VSCodeCommentary
     omap gc  <Plug>VSCodeCommentary
     nmap gcc <Plug>VSCodeCommentaryLine
+
+    highlight OperatorSandwichBuns guifg='#aa91a0' gui=underline ctermfg=172 cterm=underline
+    highlight OperatorSandwichChange guifg='#edc41f' gui=underline ctermfg='yellow' cterm=underline
+    highlight OperatorSandwichAdd guibg='#b1fa87' gui=none ctermbg='green' cterm=none
+    highlight OperatorSandwichDelete guibg='#cf5963' gui=none ctermbg='red' cterm=none
     " vscode only <
 ]]
