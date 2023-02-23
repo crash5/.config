eea0dacf533e7a65fe492415ef7a3c0c2583ea62
Imre NAGY <crash@veluna.eu>
2023-02-23 15:11:52 +0100

    vim/nvim: config cleanup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,17 +1,8 @@
 -- vim: foldmethod=marker formatoptions-=o
 
 vim.cmd [[
+    let g:loaded_python3_provider = 0
     source $XDG_CONFIG_HOME/vim/vimrc
-
-    set shadafile=$XDG_DATA_HOME/nvim/nviminfo
-    set undodir&
-
-    set inccommand=nosplit
-
-    augroup Vimrc_highlight_on_yank
-        autocmd!
-        autocmd TextYankPost * silent! lua vim.highlight.on_yank{timeout=250}
-    augroup END
 ]]
 
 
--- nvim/vscode-init.lua
+++ nvim/vscode-init.lua
@@ -1,20 +1,8 @@
 vim.cmd [[
     let g:loaded_python3_provider = 0
-
     source $XDG_CONFIG_HOME/vim/vimrc
 
-    set shadafile=$XDG_DATA_HOME/nvim/nviminfo
-    set undodir&
-
-    set inccommand=nosplit
-
-    augroup Vimrc_highlight_on_yank
-        autocmd!
-        autocmd TextYankPost * silent! lua vim.highlight.on_yank{timeout=250}
-    augroup END
-
     " vscode only >
-    packadd! targets.vim
 
     set clipboard^=unnamed,unnamedplus
 
@@ -22,7 +10,6 @@ vim.cmd [[
     nnoremap ]d <Cmd>call VSCodeNotify('editor.action.marker.next')<CR>
     nnoremap [d <Cmd>call VSCodeNotify('editor.action.marker.prev')<CR>
     nnoremap <leader>d <Cmd>call VSCodeNotify('editor.action.showHover')<CR>
-
 
     nnoremap <leader>q <Cmd>call VSCodeNotify('workbench.action.closeActiveEditor')<CR>
     nnoremap <leader>Q <Cmd>call VSCodeNotify('workbench.action.closeAllEditors')<CR>
