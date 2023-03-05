1963b1d1499b13cf809d2f2233f26a9dcaa3bcf1
Imre NAGY <crash@veluna.eu>
2023-03-05 11:56:34 +0100

    git: check for tty in git precommit check

--- git/template/hooks/pre-commit_no-direct-push-default-branch.sh
+++ git/template/hooks/pre-commit_no-direct-push-default-branch.sh
@@ -1,6 +1,9 @@
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DEFAULT_BRANCH='main|master'
 
+# If output not a tty exit
+if [ ! -t 1 ]; then exit 0; fi
+
 if [[ "$BRANCH" =~ ^($DEFAULT_BRANCH)$ ]]; then
     read -p "Commit directly to the default branch: ${BRANCH}? (y/N) " yn < /dev/tty
     case $yn in
