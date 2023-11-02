a927027a09325fbe1f9d940772de5a7f1074dd72
Imre NAGY <crash@veluna.eu>
2023-11-02 14:59:44 +0100

    bash: save command history per day next to bash history file
    source: https://spin.atomicobject.com/2016/05/28/log-bash-history/

--- bash/bashrc.d/130.bash-save-history-after-commands.sh
+++ bash/bashrc.d/130.bash-save-history-after-commands.sh
@@ -5,3 +5,4 @@
 if [[ ${PROMPT_COMMAND} != *"history -a"* ]]; then
     export PROMPT_COMMAND="history -a;${PROMPT_COMMAND}"
 fi
+export PROMPT_COMMAND='if [ "$(id -u)" -ne 0 ]; then echo "$(date "+%Y-%m-%d.%H:%M:%S") $(pwd) $(HISTTIMEFORMAT='' history 1)" >> "$XDG_DATA_HOME"/bash/bash-history-$(date "+%Y-%m-%d").log; fi;'"${PROMPT_COMMAND}"
