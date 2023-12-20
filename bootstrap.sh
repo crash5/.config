71b2bce484ee4b95c222032fab1f390a6960f41f
Imre NAGY <crash@veluna.eu>
2023-12-20 14:43:26 +0100

    bootstrap: use fix typo, use spaces, create vim dirs under state

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,44 @@
 #!/usr/bin/env bash
 
 if [[ -z "$XDG_CONFIG_HOME" ]]; then
-	echo "Please source shrc first"
-	exit
+    echo "Please source shrc first"
+    exit
 fi
 
 dirs=(
-	"$XDG_BIN_HOME"
+    "$XDG_BIN_HOME"
 
-	"$XDG_STATE_HOME/bash"
-	"$XDG_STATE_HOME/less"
-	"$XDG_STATE_HOME/python"
-	"$XDG_STATE_HOME/sqlite3"
+    "$XDG_DATA_HOME/bash"
 
-	"$XDG_DATA_HOME/vim/undo"
-	"$XDG_DATA_HOME/vim/swap"
-	"$XDG_DATA_HOME/vim/backup"
+    "$XDG_STATE_HOME/bash"
+    "$XDG_STATE_HOME/less"
+    "$XDG_STATE_HOME/python"
+    "$XDG_STATE_HOME/sqlite3"
+
+    "$XDG_STATE_HOME/vim/backup"
+    "$XDG_STATE_HOME/vim/nvimundo"
+    "$XDG_STATE_HOME/vim/swap"
+    "$XDG_STATE_HOME/vim/views"
+    "$XDG_STATE_HOME/vim/vimundo"
 )
 
 files=(
-	"$XDG_SATE_HOME/sqlite3/sqlite_history"
-	"$XDG_SATE_HOME/python/python_history"
+    "$XDG_STATE_HOME/sqlite3/sqlite_history"
+    "$XDG_STATE_HOME/python/python_history"
 )
 
 for dir in "${dirs[@]}"; do
-	mkdir -p $dir
-	echo "made $dir"
+    mkdir -p $dir
+    echo "made $dir"
 done
 
 for file in "${files[@]}"; do
-	touch $file
-	echo "touched $file"
+    touch $file
+    echo "touched $file"
 done
 
 for file in "$XDG_CONFIG_HOME/bash/utilities/"*.*; do
-	base=$(basename $(basename "$file" .sh) .py)
-	[[ -e "$XDG_BIN_HOME/$base" ]] || ln -s "$file" "$XDG_BIN_HOME/$base"
-	echo "Added utility $base"
+    base=$(basename $(basename "$file" .sh) .py)
+    [[ -e "$XDG_BIN_HOME/$base" ]] || ln -s "$file" "$XDG_BIN_HOME/$base"
+    echo "Added utility $base"
 done
