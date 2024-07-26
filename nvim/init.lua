2e35a209593e2e7a088290c70374eb425030bf8a
Imre NAGY <crash@veluna.eu>
2024-07-26 11:39:11 +0200

    nvim: disable neovide animations

--- nvim/init.lua
+++ nvim/init.lua
@@ -190,6 +190,18 @@ for newgroup, oldgroup in pairs(links) do
 end
 -- }}}
 
+
+if vim.g.neovide then
+    vim.g.neovide_position_animation_length = 0
+    vim.g.neovide_scroll_animation_length = 0
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_cursor_trail_size = 0
+    vim.g.neovide_cursor_animate_in_insert_mode = false
+    vim.g.neovide_cursor_animate_command_line = false
+    vim.g.neovide_cursor_vfx_mode = ""
+end
+
+
 --
 vim.cmd [[
 source $XDG_CONFIG_HOME/vim/common-after
