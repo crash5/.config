3afd18d189ebe1fb1d6d5324ea36508359b5f348
Imre NAGY <crash@veluna.eu>
2024-09-14 11:06:54 +0200

    nvim: show hidden files in oil.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,7 +38,11 @@ vim.cmd [[
 ]]
 
 
-require("oil").setup()
+require("oil").setup({
+    view_options = {
+        show_hidden = true
+    }
+})
 vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
 
 
